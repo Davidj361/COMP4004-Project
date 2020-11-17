500b064f9f071563641632fba7ba75d96097904e
KumamonRiceCake <jeongjeongwonkim3@cmail.carleton.ca>
2020-11-17 18:36:38 -0500

    Created project

---
+++ comp4004_project/src/main/java/Rummykub/comp4004_project/App.java
@@ -0,0 +1,13 @@
+package Rummykub.comp4004_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
