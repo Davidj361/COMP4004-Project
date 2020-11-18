41be6ffb5569d319776370c3131d25b5029b3bdb
Davidj361 <david.j.361@gmail.com>
2020-11-18 15:45:19 -0500

    refactored to tiles

--- code/src/main/java/Rummykub/Deck.java
+++ code/src/main/java/Rummykub/Deck.java
@@ -5,16 +5,16 @@ import java.util.Collections;
 import static Rummykub.Tile.Colors;
 
 public class Deck{
-    private ArrayList<Tile> totalDeck;
+    private ArrayList<Tile> tiles;
 
     //Create full deck of all tiles
     public Deck() {
-        totalDeck = new ArrayList<Tile>();
+        tiles = new ArrayList<Tile>();
         for (int j = 0; j < 4; j++) {
             for (int x = 0; x < 2; x++){
                 for (int i = 1; i < 14; i++) {
                     Tile tile = new Tile(i, Colors.values()[j]);
-                    totalDeck.add(tile);
+                    tiles.add(tile);
                 }
             }
         }
@@ -23,22 +23,22 @@ public class Deck{
     }
 
     //get the full deck
-    public ArrayList<Tile> getTotalDeck(){
-        return totalDeck;
+    public ArrayList<Tile> getTiles(){
+        return tiles;
     }
 
     public int size() {
-        return totalDeck.size();
+        return tiles.size();
     }
 
     //remove tile from deck
     public Tile dealTile() {
-        Tile dealtTile = totalDeck.remove(0);
+        Tile dealtTile = tiles.remove(0);
         return dealtTile;
     }
 
     //shuffle tiles in the deck
     public void shuffleTiles() {
-        Collections.shuffle(totalDeck);
+        Collections.shuffle(tiles);
     }
 }
