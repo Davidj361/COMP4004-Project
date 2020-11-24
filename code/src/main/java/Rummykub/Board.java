d1a7eb5bc2da6149a3e55193798c07b88ddc1bd5
brad888c <35773152+brad888c@users.noreply.github.com>
2020-11-24 14:39:55 -0500

    CODE - Added interaction with board

--- code/src/main/java/Rummykub/Board.java
+++ code/src/main/java/Rummykub/Board.java
@@ -1,24 +1,53 @@
 package Rummykub;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Board {
-	private Tile [][] board;
-	
+	protected ArrayList<ArrayList<Tile>> board = new ArrayList<ArrayList<Tile>>();
+
 	public Board() {
-		board = new Tile[100][100];
+
 	}
-	
-	public boolean putTiles(ArrayList<Tile> tiles, int row, int col) {
-		Tile[][] tempBoard = board;
-		for (int i=0; i< tiles.size(); i++) {
-			if (row+i > 99 || col > 99)
-				return false;
-			if (tempBoard[row+i][col] != null)
-				return false;
-			tempBoard[row+i][col] = tiles.get(i);
+
+	//Add a new set to the board
+	public void addSet(ArrayList<Tile> set) {
+		board.add(set);
+	}
+
+	//Add specific tiles from hand to board
+	public void addToCurrent(ArrayList<Tile> tiles, int row){
+		board.get(row).addAll(tiles);
+		Collections.sort(board.get(row), new Comparator<Tile>() {
+			@Override
+			public int compare(Tile tile2, Tile tile1)
+			{
+				return  tile1.getValue().compareTo(tile2.getValue());
+			}
+		});
+	}
+
+	//Seperates a current set on the board into two sets
+	public void seperateSet(int row, int tileNum){
+		ArrayList<Tile> newSet = new ArrayList<Tile>();
+		for (int i = row; i < board.get(tileNum).size(); i++) {
+			newSet.add(board.get(tileNum).get(i));
 		}
-		board = tempBoard;
-		return true;
+		board.add(newSet);
+	}
+
+	//combine two sets thats are currently on the board
+	public void combineCurrent(int row1, int row2){
+		ArrayList<Tile> combine = board.get(row2);
+		board.remove(row2);
+		board.get(row1).addAll(combine);
+		Collections.sort(board.get(row1), new Comparator<Tile>() {
+			@Override
+			public int compare(Tile tile2, Tile tile1)
+			{
+				return  tile1.getValue().compareTo(tile2.getValue());
+			}
+		});
 	}
 }
