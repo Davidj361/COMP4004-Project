12072a49725af5d2db50fc7a9488a1ff1c51871d
Davidj361 <david.j.361@gmail.com>
2020-11-27 22:54:50 -0500

    Added command handler Added some functions called by command handler Added curPlayer and curPlayerName to give the index of players on who's turn it is Added constructors to Game

--- code/src/main/java/Rummykub/Game.java
+++ code/src/main/java/Rummykub/Game.java
@@ -11,17 +11,51 @@ public class Game {
     public Deck deck;
     private Board board;
     private Hand hand;
+	// players and clients indices should match
+	// i.e. client[0] -> player[0]
+	private ArrayList<Player> players = new ArrayList<Player>();
     Scanner scanner = new Scanner(System.in);
+    private boolean testing; // A useful flag for code
 
-    public Game() {
-    }
 
-    //private ArrayList<Player> players = new ArrayList<Player>();
+	// Constructors
+	public Game() {
+		this(false);
+	}
+	public Game(boolean b) {
+		players.add(Player("local"));
+		testing = b;
+		reset();
+	}
+	public Game(int i) {
+		this(i, false);
+	}
+	public Game(int i, boolean b) {
+		testing = b;
+		numPlayers = i;
+		reset();
+	}
+	public Game(Server s) {
+		this(s, false);
+	}
+	public Game(Server s, boolean b) {
+		testing = b;
+		server = s;
+		server.game = this;
+		//numPlayers = server.clientsConnected+1;
+		reset();
+	}
+
+
+	// TODO Reset all variables
+	// Useful for testing and restarting the game
+	public void reset() {
+	}
+
     public void startGame() {
         gameRunning = true;
         deck = new Deck();
         board = new Board();
-
 
         // Add all players
         for (int i = 0; i < 3; i++) {
@@ -112,41 +146,81 @@ public class Game {
     }
      */
 
+	// Get who's the current player this turn for indexing purposes
+	public int curPlayer() {
+		return ((turn-1) % players.size());
+	}
+	public String curPlayerName() {
+		return players.get(curPlayer()).getName();
+	}
+
+	// Prints the same string to all players
+	public void print(String str) {
+		if (server != null)
+			server.print(str);
+		else
+			System.out.print(str);
+	}
+
 	public boolean command(int player, String input) {
 		if (!playerTurn(player))
 			return false;
 
-		//if (input.equals("quit"))
-		if (input.charAt(0) == 'h') // help text
-			help();
-		else if (input.charAt(0) == 'd') // display the board
-			displayBoard();
-		else if (input.charAt(0) == 'h') // display the board
-			displayHand();
-		else if (input.charAt(0) == 'u') // undo
-			undo();
-		else if (input.charAt(0) == 'e') // end turn
-			endTurn();
-		else if (input.length() > 1) { // Commands with input
-			if (input.charAt(0) == 'p') { // placing tiles from hand onto the board
-				place(input);
-			} else if (input.charAt(0) == 'g') { // giving tiles to a row on the board
-				give(input);
-			} else if (input.charAt(0) == 'm') { // moving tiles from one row to another on the board
-				move(input);
-			} else if (input.charAt(0) == 's') { // splitting rows on the board
-				split(input);
+		String[] sArr = input.split(" ");
+		if (input.length() > 1) { // Commands with input arguments
+			switch(sArr[0]) {
+				case "h": // display player's hand
+					help();
+					break;
+				case "db": // display the board
+					board.printBoard();
+					break;
+				case "dh": // display player's hand
+					players[i].hand.printHand();
+					break;
+				case "u": // undo
+					undo();
+					break;
+				case "e": // end turn
+					endTurn();
+					break;
+			}
+		} else { // No arguments to commands
+			switch(sArr[0]) {
+				case "p": // placing tiles from hand onto the board
+					placeTiles(sArr);
+					break;
+				case "g": // giving tiles to a row on the board
+					giveTiles(sArr);
+					break;
+				case "m": // moving tiles from one row to another on the board
+					moveTiles(sArr);
+					break;
+				case "s": // splitting rows on the board
+					splitRow(sArr);
+					break;
 			}
 		}
 		return true;
 	}
 
-	public boolean help() {
+	// Print from the help from a file
+	// TODO Have a help file and read from it
+	private void help() {
+		print("WIP help");
 	}
 
-	public boolean displayBoard() {
+	// Reverts the player's hand and the board to the original state
+	// as when the turn started
+	// TODO Get origHand and origBoard initialized at every start of a turn
+	private boolean undo() {
+		hand = origHand;
+		board = origBoard;
+		return true;
 	}
 
-	public boolean di() {
+	// Places tiles from active player's hand to the board
+	private boolean placeTiles(String[] sArr) {
+		return true;
 	}
 }
