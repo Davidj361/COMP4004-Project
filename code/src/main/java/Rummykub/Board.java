ba21f8b2314a11f0fe680e0447ca05fec92ee614
brad888c <35773152+brad888c@users.noreply.github.com>
2020-11-29 17:05:37 -0500

    REFACTOR - Fixed issues with validating as well as ordering

--- code/src/main/java/Rummykub/Board.java
+++ code/src/main/java/Rummykub/Board.java
@@ -21,27 +21,31 @@ public class Board {
 		board.get(row).addAll(tiles);
 		Collections.sort(board.get(row), new Comparator<Tile>() {
 			@Override
-			public int compare(Tile tile2, Tile tile1){return  Integer.compare(tile1.getValue(), tile2.getValue());}
+			public int compare(Tile tile1, Tile tile2){return  Integer.compare(tile1.getValue(), tile2.getValue());}
 		});
 	}
 
-	//Seperates a current set on the board into two sets
-	public void seperateSet(int row, int tileNum){
+	//Separates a current set on the board into two sets
+	public void separateSet(int row, int tileNum){
 		ArrayList<Tile> newSet = new ArrayList<Tile>();
-		for (int i = row; i < board.get(tileNum).size(); i++) {
-			newSet.add(board.get(tileNum).get(i));
+		for (int i = tileNum; i < board.get(row).size(); i++) {
+			newSet.add(board.get(row).get(i));
+		}
+		int size = board.get(row).size();
+		for(int i = tileNum; i < size; i++){
+			board.get(row).remove(tileNum);
 		}
 		board.add(newSet);
 	}
 
-	//combine two sets thats are currently on the board
+	//combine two sets that's are currently on the board
 	public void combineCurrent(int row1, int row2){
 		ArrayList<Tile> combine = board.get(row2);
 		board.remove(row2);
 		board.get(row1).addAll(combine);
 		Collections.sort(board.get(row1), new Comparator<Tile>() {
 			@Override
-			public int compare(Tile tile2, Tile tile1){return  Integer.compare(tile1.getValue(), tile2.getValue());}
+			public int compare(Tile tile1, Tile tile2){return  Integer.compare(tile1.getValue(), tile2.getValue());}
 		});
 	}
 
@@ -62,7 +66,7 @@ public class Board {
 			}
 			printBoard += "\n";
 		}
-		for(int i = 0; i < ((size * 9) + 1); i++) {
+		for(int i = 0; i < ((size * 18) + 1); i++) {
 			printBoard+="-";
 		}
 		return printBoard;
@@ -73,18 +77,23 @@ public class Board {
 		for (int i = 0; i < board.size(); i++) {
 			int type = 0;
 			int valueCorrect = 0;
+			ArrayList<Tile.Colors> colors = new ArrayList<Tile.Colors>();
+			if(board.get(i).size() < 3){
+				return false;
+			}
 			for (int j = 0; j < board.get(i).size() - 1; j++) {
-				if(board.get(i).get(j).getValue() == board.get(i).get(j + 1).getValue() - 1 && board.get(i).get(0).getColor() == board.get(i).get(j+1).getColor() && (type == 0 || type == 1)) {
+				colors.add(board.get(i).get(j).color);
+				if(board.get(i).get(j).getValue() == board.get(i).get(j + 1).getValue() - 1 && board.get(i).get(j).getColor() == board.get(i).get(j+1).getColor() && (type == 0 || type == 1)) {
 					valueCorrect++;
 					type = 1;
 				} else {
-					if(board.get(i).get(j).getValue() == board.get(i).get(j + 1).getValue() && board.get(i).get(j).getColor() != board.get(i).get(j+1).getColor() && (type == 0 || type == 2)){
+					if(board.get(i).get(j).getValue() == board.get(i).get(j + 1).getValue() && !colors.contains(board.get(i).get(j+1).getColor()) && (type == 0 || type == 2)){
 						valueCorrect++;
 						type = 2;
 					}
 				}
 			}
-			if(valueCorrect != board.get(i).size()){
+			if(valueCorrect != board.get(i).size()-1){
 				return false;
 			}
 		}
