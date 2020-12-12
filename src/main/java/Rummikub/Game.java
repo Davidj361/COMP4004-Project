777dee749acc963b09784969e50a4193f93ed4fb
brad888c <35773152+brad888c@users.noreply.github.com>
2020-12-12 18:05:11 -0500

    REFACTOR - Fixed bug and printing

--- src/main/java/Rummikub/Game.java
+++ src/main/java/Rummikub/Game.java
@@ -474,6 +474,7 @@ public class Game {
 		int[] tilesIdx = new int[sArr.length-1];
 		for (int i=1; i<sArr.length; i++)
 			tilesIdx[i - 1] = Integer.parseInt(sArr[i]);
+			Arrays.sort(tilesIdx);
 		if (player.hasTiles(tilesIdx)) {
 			ArrayList<Tile> playerTiles = player.placeTiles(tilesIdx);
 			board.addToCurrent(playerTiles,dstRow);
